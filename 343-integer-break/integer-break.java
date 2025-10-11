87c1a13ab2ec1b79bf425492ed66b63d5e09d3fa
durganandakishore <147476030+durganandakishore@users.noreply.github.com>
2025-10-11 12:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (21.96%) - LeetSync

---
+++ 343-integer-break/integer-break.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n<4){
+            n -= 1;
+        }
+        int ans = 1;
+        while(n>4){
+            ans *= 3;
+            n -= 3;
+        }
+        return ans *n;
+
+    }
+}
